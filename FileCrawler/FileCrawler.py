591bd5bae51199ca3ce3466571113da0d3f57039
MARK LUTTRELL <marktylerluttrell@gmail.com>
2018-12-24 00:49:29 -0600

    Added Spacing

--- FileCrawler/FileCrawler.py
+++ FileCrawler/FileCrawler.py
@@ -2,10 +2,16 @@ import os
 import shutil
 import sys
 
+
+print("\nUse can use this program to scan drives and find specific file types. Simply add file extension types to the 'ShortExtensionList.txt' file in this program's folder.  \n")
+
 sFileTypes = open("ShortExtensionList.txt").read().splitlines()
 sUserDestination = input(r"Where do you want to put the located files? For Example, 'C:\Users\YourUsername\Desktop\NewFolder' without quotes: ")
-sUserInput = input("Enter the drive you want to scan. For example, 'C:' without quotes: ")
-sUserNamedLog = input("Name your log, so you know what it's called. For example, 'MyLog.txt' without quotes. It will be located in the same folder as this program. You'll need this to reference the original file paths: ")
+
+sUserInput = input("\nEnter the drive you want to scan. For example, 'C:' without quotes: ")
+
+sUserNamedLog = input("\nName your log, so you know what it's called. For example, 'MyLog.txt' without quotes. It will be located in the same folder as this program. You'll need this to reference the original file paths: ")
+print("\n")
 
 #sUserLogDestination = input(r"Make a .txt file and provide its location for the log. For example, 'C:\Users\YourUsername\Desktop\NewFolder\Log.txt': ")
 
